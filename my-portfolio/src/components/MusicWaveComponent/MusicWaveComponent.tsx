d66f80752fed096d53fa39d8064f37f2e8f7d96a
Nemanja Andric <nemanja94@users.noreply.github.com>
2022-12-06 08:52:45 +0000

    Added musicWaveComponent

---
+++ my-portfolio/src/components/MusicWaveComponent/MusicWaveComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const MusicWaveComponent = () => {
+  return (
+    <div className="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default MusicWaveComponent;
